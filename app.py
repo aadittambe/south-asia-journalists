9201d65776373ca3ff682de2837e502b064fe880
Aadit Tambe <aadit.tambe@gmail.com>
2021-10-23 02:48:01 -0400

    works

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import render_template
 app = Flask(__name__)
 
 def get_csv():
-    csv_path = './static/final_clean.csv'
+    csv_path = './static/journalists.csv'
     csv_file = open(csv_path, 'r')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
